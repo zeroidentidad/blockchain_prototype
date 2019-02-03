0622b080c9d23caaa4ae718053290246b7183b03
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-02-03 01:40:22 -0600

    avance
    definiendo red de nodos

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const uuid = require('uuid/v1');
+const port = process.argv[2];
 
 const nodeAddress = uuid().split('-').join('');
 
@@ -43,6 +44,6 @@ app.get('/mine', function(req, res){
 
 });
 
-app.listen(3000, function(){
-	console.log('Listen on port');
+app.listen(port, function(){
+	console.log(`Listen on port ${port} ...`);
 });
